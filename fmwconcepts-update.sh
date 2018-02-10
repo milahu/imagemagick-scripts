e2078a7f2af102d89000d040581b62b367127993
Anton P. Linevich <anton@linevich.com>
2018-02-10 10:55:16 +0200

    sync with master site

--- fmwconcepts-update.sh
+++ fmwconcepts-update.sh
@@ -2,8 +2,8 @@
 cd $(dirname $0)
 cd ./bin/
 wget 'http://www.fmwconcepts.com/imagemagick/script_list.txt' -O script_list.txt
-awk -F\-\- '{ print $1}' script_list.txt | egrep '^\w' | while read name; do
-    wget "http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=${name}&dirname=${name}" -O ${name}
+awk -F\- '{ print $1}' script_list.txt | egrep '^\w' | while read name; do
+    wget -c "http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=${name}&dirname=${name}" -O ${name}
     chmod +x ${name}
 done
 git add *
